d75b4ebca8bef290b8f9ddaf10236375731b28f2
Lucas Gabriel Babolim Vieira <lucasvieira0420@gmail.com>
2025-03-30 16:46:32 -0300

    Boas vindas páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css'
+import img1 from './assets/imagem-1.png'
+import foguete from './assets/foguete.png'
+import ButtomSalle from './components/ButtomSalle'
+
 
 function App() {
 
   return (
     <>
+      <main className="container">
+        <section className='boas-vindas'>
+          <div className='boas-vindas-txt'>
+            <h1 className='mg40-bottom'>Feche pelo menos 3 contratos de R$1.500 nos próximos 30 dias <img className='img-icone' src={foguete} alt="" /></h1>
+            <p className='mg40-top mg40-bottom'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Accusamus in corrupti cumque sit natus neque facilis unde dolores itaque similique obcaecati, officia atque veniam. 
+              Quis commodi voluptatibus rerum provident officiis.</p>
+            <ButtomSalle />
+            <p className='promocao-txt'>
+              Promo fim de ano!
+              <br />
+              De <s>R$453,00</s> por apenas <strong>R$97,00</strong>
+            </p>
+          </div>
+          <div >
+            <img className='img-container' src={img1} alt="Logo Salle" />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ src/components/ButtomSalle/index.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function ButtomSalle() {
+  return (
+    <div>
+      <div className="buttom">
+        <p className='bottom-txt'>Quero me <strong>inscrever agora</strong></p>
+      </div>
+    </div>
+  );
+}
+
+export default ButtomSalle;
