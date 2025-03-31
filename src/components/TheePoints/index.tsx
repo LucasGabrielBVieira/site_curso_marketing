e9f660ca6d8820fa18c9717d179ef9bab0e7a44b
Lucas Gabriel Babolim Vieira <lucasvieira0420@gmail.com>
2025-03-31 09:26:46 -0300

    Finalização do ThreePoints

--- src/components/TheePoints/index.tsx
+++ src/components/TheePoints/index.tsx
@@ -1,28 +1,74 @@
 import './style.css'
+import { useState } from 'react'
 import modulo from '../../assets/modulo.png'
+import moduloHover from '../../assets/modulo-hover.png'
 import comunidade from '../../assets/comunidade.png'
+import comundidadeHover from '../../assets/comunidade-hover.png'
 import suporte from '../../assets/suporte.png'
+import suporteHover from '../../assets/suporte-hover.png'
 
 function TheePoints() {
+
+    const [hoveredModulo, setHoveredModulo] = useState(false);
+    const [hoveredComunidade, setHoveredComunidade] = useState(false);
+    const [hoveredSuporte, setHoveredSuporte] = useState(false);
+
+    const originalModulo = modulo;
+    const originalComunidade = comunidade;
+    const originalSuporte = suporte;
+
+    const hoverModulo = moduloHover;
+    const hoverComunidade = comundidadeHover;
+    const hoverSuporte = suporteHover;
+
+    const handleMouseEnterModulo = () => {
+        setHoveredModulo(true);
+    };
+    const handleMouseLeaveModulo = () => {
+        setHoveredModulo(false);
+    };
+
+    const handleMouseEnterComunidade = () => {
+        setHoveredComunidade(true);
+    };
+    const handleMouseLeaveComunidade = () => {
+        setHoveredComunidade(false);
+    };
+
+    const handleMouseEnterSuporte = () => {
+        setHoveredSuporte(true);
+    };
+    const handleMouseLeaveSuporte = () => {
+        setHoveredSuporte(false);
+    };
+
     return (
         <>
             <div className='container-three-points'>
-                <div className='container-moldura point-1'>
-                    <img src={modulo} alt="" />
+                <div
+                    onMouseMove={handleMouseEnterModulo}
+                    onMouseLeave={handleMouseLeaveModulo}
+                    className='container-moldura point-1'>
+                    <img src={hoveredModulo ? hoverModulo : originalModulo} alt="" />
                     <p>9 módulos disponíveis</p>
                 </div>
-                <div className='container-moldura point2'>
-                    <img src={comunidade} alt="" />
+                <div onMouseMove={handleMouseEnterComunidade}
+                    onMouseLeave={handleMouseLeaveComunidade}
+                    className='container-moldura point-1'>
+                    <img src={hoveredComunidade ? hoverComunidade : originalComunidade} alt="" />
                     <p>Comunidade VIP</p>
                 </div>
-                <div className='container-moldura point3'>
-                    <img src={suporte} alt="" />
+                <div
+                    onMouseMove={handleMouseEnterSuporte}
+                    onMouseLeave={handleMouseLeaveSuporte}
+                    className='container-moldura point3'>
+                    <img src={hoveredSuporte ? hoverSuporte : originalSuporte} alt="" />
                     <p>Equipe de suporte</p>
                 </div>
-            </div>
-            <div className='three-points-txt'>
-                <p>✔Você vai aprender a posicionar a sua empresa no mercado digital</p>
-                <p>✔Ensinamos várias ferramentas para turbinar seu marketing</p>
+                <div className='three-points-txt'>
+                    <p className='three-points-txt-p1'>✔Você vai aprender a posicionar a sua empresa no mercado digital</p>
+                    <p className='three-points-txt-p2'>✔Ensinamos várias ferramentas para turbinar seu marketing</p>
+                </div>
             </div>
         </>
     )
